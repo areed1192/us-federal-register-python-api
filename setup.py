bc02daf084275f67b4501f5e6ec50f9513a7fcec
Alex Reed <alexreed1192@gmail.com>
2020-08-11 16:39:41 -0700

    add initial changes

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+from setuptools import find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
